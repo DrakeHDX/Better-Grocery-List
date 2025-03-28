29c3f15fcd9369ff0077df6f89113da386c731d7
DrakeHDX <143469538+DrakeHDX@users.noreply.github.com>
2025-03-27 21:37:49 -0400

    Add files via upload

---
+++ API/Grocery.cs
@@ -0,0 +1,27 @@
+﻿namespace MyAPI.Data.Models
+
+{
+    public class Grocery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+
+        public Grocery(int id, string name)
+        {
+            Id = id;
+            Name = name;
+
+        }
+
+        public static List<Grocery> GetItems() => new List<Grocery>
+        {
+            new Grocery(0, "eggs" ),
+            new Grocery(1, "milk"),
+            new Grocery(2, "bread"),
+            new Grocery(3, "cheese"),
+            new Grocery(4, "flour"),
+            new Grocery(5, "soda"),
+            new Grocery(6, "toothpaste")
+        };
+    }
+}
---
+++ API/GroceryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAPI.Data.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroceryController : ControllerBase
+    {
+        private static readonly List<Grocery> _grocery = Grocery.GetItems();
+        private readonly ILogger _logger;
+
+        public GroceryController(ILogger<Grocery> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetGroceryList")]
+        public IEnumerable Get() => Grocery.GetItems();
+    }
+}
